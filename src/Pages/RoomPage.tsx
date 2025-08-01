f3f86d8d211af88effe726eeb717471394f210ba
riuteshyaaa <riteshyaaa@gmail.com>
2025-08-01 22:54:30 +0530

    Room Creation logic with user joining and there record being joining added

--- src/Pages/RoomPage.tsx
+++ src/Pages/RoomPage.tsx
@@ -4,14 +4,15 @@ import { SocketContext } from "../Context/SocketContext";
 
 const RoomPage : React.FC = () => {
     const {roomId} = useParams();
-    const {socket} = useContext(SocketContext)
+    const {socket, user} = useContext(SocketContext)
    useEffect( () => {
         
          // Emitting this event so that either creater of the room or the user who joins the room can be identified
          // Anyone who added the server Knows that new people have been  added 
          //to the room 
-         socket.emit('joinedRoom', {roomId : roomId});
-   },[])
+         if(user){
+         socket.emit('joinedRoom', {roomId : roomId, peerId: user?.id});}
+   },[roomId, user])
 
     return(
         <div className="flex items-center justify-center h-screen text-color-white bg-gray-800"> 
--- src/context/socketContext.tsx
+++ src/context/socketContext.tsx
@@ -1,31 +1,52 @@
-import SocketIoClient from 'socket.io-client';
-import { createContext, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-const ws_server = "http://localhost:5000"
+import SocketIoClient from "socket.io-client";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Peer } from "peerjs";
+import { v4 as UUIDV4 } from "uuid";
+const ws_server = "http://localhost:5000";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, react-refresh/only-export-components
 export const SocketContext = createContext<any | null>(null);
- const socket = SocketIoClient(ws_server);
+const socket = SocketIoClient(ws_server);
 
- interface Props { 
-    children: React.ReactNode
- }
-
-export const SocketProvider: React.FC<Props> = ({ children}) =>{
-    const navigate = useNavigate(); // This is used to navigate to different pages
-    useEffect(() => {
-        const enterRoom =  ({roomId}: { roomId: string }) => {
-            console.log("Entering room with ID:", {roomId});
-            navigate(`/room/${roomId}`);
-        }
-        //  we are listening for the "roomCreated" event from the server
-        // and when it is emitted, we will call the enterRoom function  
-
-         socket.on("roomCreated", enterRoom);
-    },[])
-    
-    return (
-        <SocketContext.Provider value={{socket}}>
-            {children}
-        </SocketContext.Provider>
-    )
+interface Props {
+  children: React.ReactNode;
 }
+
+export const SocketProvider: React.FC<Props> = ({ children }) => {
+  const navigate = useNavigate(); // This is used to navigate to different pages
+  //state variable to store userId
+  const [user, setUser] = useState<Peer>();
+  // Function to fetch user data
+  const fetchUserData = ({
+    roomId,
+    participants,
+  }: {
+    roomId: string;
+    participants: string[];
+  }) => {
+    console.log("Fetching user data");
+    console.log(roomId, participants);
+  };
+  // Here you can implement the logic to fetch user data based on roomId and peerId
+  useEffect(() => {
+    const userId = UUIDV4();
+    const newPeer = new Peer(userId);
+    setUser(newPeer);
+
+    const enterRoom = ({ roomId }: { roomId: string }) => {
+      console.log("Entering room with ID:", { roomId });
+      navigate(`/room/${roomId}`);
+    };
+    //  we are listening for the "roomCreated" event from the server
+    // and when it is emitted, we will call the enterRoom function
+
+    socket.on("roomCreated", enterRoom);
+    socket.on("getUser", fetchUserData);
+  }, []);
+
+  return (
+    <SocketContext.Provider value={{ socket, user }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
